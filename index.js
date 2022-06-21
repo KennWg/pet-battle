69915174497010deb6c55a7ec90a00c01f2d8bc0
KW <kenneth_sl_wong@msn.com>
2022-06-21 19:43:16 -0400

    added character sprites, adjusted map, added drawing of map onto screen

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const canvas = document.querySelector('canvas');
+const con = canvas.getContext('2d');
 
 canvas.width = 1024;
 canvas.height = 576;
+
+con.fillStyle = 'white';
+con.fillRect(0, 0, canvas.width, canvas.height);
+
+const image = new Image();
+image.src = './images/PetBattle.png'
+
+image.onload = () => {
+    con.drawImage(image, -500, -1100);
+};
